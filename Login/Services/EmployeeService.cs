53e848b9bf22e2b201cede85cfe99b132c260e8c
bpj0sgw <jonathanclark@ups.com>
2020-11-22 21:24:24 -0500

    cleaned up UI Navigation and added a service not yet implemented

---
+++ Login/Services/EmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Login.Models;
+
+namespace Login.Services
+{
+    public class EmployeeService
+    {
+        public EmployeeService()
+        {
+        }
+
+        //public int DeleteEmployee(LoginModel loginModel)
+        //{
+
+        //}
+    }
+}
---
+++ Login/Views/Registration.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Login.Views
+{
+    public partial class Registration : ContentPage
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+       void OnAdd(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
